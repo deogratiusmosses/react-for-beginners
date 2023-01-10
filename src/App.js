8e68064b5f88dd3c583df2ec96df369536a9c72a
Deogratius Mosses <caldroncloud.10@gmail.com>
2023-01-10 12:55:52 +0300

    complete effects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import Button from './Button'
-import styles from "./Title.module.css"
+import { useState, useEffect } from 'react'
 
+function Hello(){
+  useEffect(()=>{
+    console.log('im here')
+  },[])
+  return <h1>Hello</h1>
+
+}
 
 function App() {
+  const [showing, setShowing]= useState(false)
+  const onClick=()=>setShowing(prev => !prev)
   return (
-    <div className="App">
-      <h1 className={styles.title}>Welcome To React</h1>
-      <Button text={'Continue'} />
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
     </div>
   )
 }
--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,12 @@
-import propTypes from "prop-types"
-import styles from"./Button.module.css"
+// import propTypes from "prop-types"
+// import styles from"./Button.module.css"
 
-function Button ({text}){
-  return <button className={styles.button}>{text}</button>
-}
+// function Button ({text}){
+//   return <button className={styles.button}>{text}</button>
+// }
 
-Button.propTypes={
-  text: propTypes.string.isRequired
-}
+// Button.propTypes={
+//   text: propTypes.string.isRequired
+// }
 
-export default Button
+// export default Button
